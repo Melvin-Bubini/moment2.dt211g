feaff565e8bd3b53b73d83a2c7e771c084838de9
Melvin Bubini <melvin.bubini@gmail.com>
2024-01-26 13:22:11 +0100

    färdiga functioer för funktionerna

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -586,6 +586,16 @@ async function init() {
     try {
         const response = await fetch(url);
         const kurser = await response.json();
+        // sortera data utifårn kursnamn
+        // kurser.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1);
+        // sortera data utifrån kurskod
+        // kurser.sort((a, b) => (a.code > b.code) ? 1 : -1);
+        // sortera data utifrån progression
+        // kurser.sort((a, b) => (a.progression > b.progression) ? 1 : -1);
+        // filtrera data
+        // let filteredKurser = kurser.filter((kurs) => {
+        //     return kurs.coursename.toLowerCase().includes("we");
+        // });
         displayKurser(kurser);
     } catch  {
         document.getElementById("error").innerHTML = "<p>N\xe5got gick fel</p>";
--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,21 @@ async function init() {
         const response = await fetch(url);
         const kurser = await response.json();
 
+        // sortera data utifårn kursnamn
+        // kurser.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1);
+
+        // sortera data utifrån kurskod
+        // kurser.sort((a, b) => (a.code > b.code) ? 1 : -1);
+
+        // sortera data utifrån progression
+        // kurser.sort((a, b) => (a.progression > b.progression) ? 1 : -1);
+
+        // filtrera data
+        // let filteredKurser = kurser.filter((kurs) => {
+        //     return kurs.coursename.toLowerCase().includes("we");
+        // });
+
+
         displayKurser(kurser)
     } catch {
         document.getElementById("error").innerHTML = "<p>Något gick fel</p>";
